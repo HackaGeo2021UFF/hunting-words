0f0cd84ed4eace918b87972f4669f4b821e1f6a1
Leonardo Miquelutti <lmiquelutti@id.uff.br>
2021-03-12 10:52:13 -0300

    HACKAGEO-18 #done

--- test.py
+++ test.py
@@ -53,4 +53,4 @@ if __name__ == '__main__':
     import sys
     path_input = sys.argv[1]    # exemple  <- data/input_N.txt
     path_output = sys.argv[2]   # exemple  <- data/output_N.txt
-    test(path_input, path_output)
+    print(test(path_input, path_output))
