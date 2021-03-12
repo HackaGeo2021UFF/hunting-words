828f15050515bb63bac2cb8c444f9eba1f4c575c
Leonardo Miquelutti <lmiquelutti@id.uff.br>
2021-03-12 09:34:52 -0300

    HACKAGEO-18

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ def test(pathInput, pathOutput):
     '''
     Description
         Test challenge.
+        Requires the function challenge(words, matrix)
+        to return the result in the same order of the words
+        input argument
     Arguments
         pathInput: string
         pathOutput: string
@@ -11,7 +14,40 @@ def test(pathInput, pathOutput):
         result: bool
     '''
     # code
-    return None
+
+    # 1 - read input dataset
+    with open(pathInput, 'r') as g:
+        f = g.readlines()
+
+    # 2 - prepare input dataset
+    nWords, nRows, nCols = [int(s) for s in f[0].split()]
+
+    words = []
+    for i in range(nWords):
+        words.append(f[i + 1].strip('\n'))
+
+    matrix = []
+    aux = nWords + 1
+    for i in range(nRows):
+        matrix.append(list(f[i + aux].strip('\n')))
+
+    # 3 - call hunting-words.py
+    result = challenge(words, matrix)
+    # result = {'google': 4, 'morosidade':1, 'coragem':2}
+
+    # 4 - compare outputs
+    with open(pathOutput, 'r') as g:
+        f = g.readlines()
+
+    control = []
+    for i in f:
+        control.append(int(i))
+
+    validation = 0
+    for i in range(nWords):
+        validation += (control[i] == result[words[i]])
+
+    return validation == nWords
 
 if __name__ == '__main__':
     import sys
