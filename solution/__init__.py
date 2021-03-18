ddd5f7294178101fcddc6f66c00447a166f679e2
Gustavo <gdssouza@id.uff.br>
2021-03-18 10:47:00 -0300

    feature: boa pratica de prog -> init sem funcoes, somente imports

--- solution/__init__.py
+++ solution/__init__.py
@@ -1,17 +1,1 @@
-from solution.auxiliary_functions import validation
-
-def challenge(words, matrix):
-    '''
-    Description
-        Hunting words.
-            * 1 word "X" in main diagonal
-            * 2 word "X" above the main diagonal
-            * 3 word "X" bellow the main diagonal
-            * 4 word "X" doesn't exist
-    Arguments
-        words: list
-        matrix: list of lists NxN
-    Return
-        result: dic {Key: word (str), Value: resposta (int)}
-    '''
-    return {}
+from solution.challenge import challenge
